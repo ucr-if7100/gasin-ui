43dc99fd138ef955ea369f935ddbdd420057095e
Jesús Antonio Gutiérrez Matarrita <jesus.gutierrezm10@outlook.com>
2023-05-29 18:52:09 -0600

    Dashboard added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,39 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatTableModule } from '@angular/material/table';
+
+import {RouterModule} from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component'
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    SidebarComponent,
+    /*TransactionsComponent,
+    ProfileComponent,
+    BankAccountComponent*/
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    AppRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTableModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,48 @@
+import { AfterViewInit, Component, OnDestroy, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  isSidebarOpen = true;
+  sidebarMode: 'side' | 'over' = 'side';
+  private resizeSubscription: Subscription | undefined;
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+    this.sidebarMode = this.isSidebarOpen ? 'side' : 'over';
+    if (!this.isSidebarOpen && this.isMobile()) {
+      this.sidenav.close();
+    } else {
+      this.sidenav.open();
+    }
+  }
+
+  ngAfterViewInit() {
+    this.sidenav.opened = this.isSidebarOpen;
+    this.sidenav.mode = this.sidebarMode;
+
+    this.resizeSubscription = window.addEventListener('resize', () => {
+      if (this.isMobile() && !this.isSidebarOpen) {
+        this.sidenav.close();
+      } else if (!this.isMobile() && !this.isSidebarOpen) {
+        this.sidenav.open();
+      }
+    }) as unknown as Subscription;
+  }
+
+  ngOnDestroy(): void {
+    if (this.resizeSubscription) {
+      this.resizeSubscription.unsubscribe();
+    }
+  }
+
+  isMobile() {
+    return window.innerWidth <= 768;
+  }
+}
