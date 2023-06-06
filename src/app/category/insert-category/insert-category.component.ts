37b4fd7ac68dde4efec8cb843601f4267028d72f
joansj21 <joansj21@gmail.com>
2023-06-05 21:31:38 -0600

    cargar categorias

--- src/app/category/insert-category/insert-category.component.ts
+++ src/app/category/insert-category/insert-category.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CategoryService } from 'src/app/services/category-service/category.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-insert-category',
@@ -6,5 +9,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./insert-category.component.css']
 })
 export class InsertCategoryComponent {
+
+
+  CategoryForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder,private rest:CategoryService) {
+
+   }
+
+  ngOnInit(): void {
+    this.CategoryForm = this.fb.group({
+      id: 1,
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      id_User: [1]
+
+    });
+  }
+
+
+  add() {
+
+    if (!this.CategoryForm.valid) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+      });
+      return;
+    }
+
+    return this.rest.add(this.CategoryForm.value).subscribe((result) => {
+
+      this.CategoryForm = this.fb.group({
+
+        description: [''],
+        name: [''],
+
+      });
+      Swal.fire(
+        'Good job!',
+        'User added sucessfully!',
+        'success'
+      )
+    }, (err) => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+      });
+    });
+  }
 
 }
--- src/app/services/category-service/category.service.ts
+++ src/app/services/category-service/category.service.ts
@@ -1,9 +1,35 @@
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, of, tap } from 'rxjs';
+
+
+const Endpoint='http://localhost:8084/api/Transaction';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(private http: HttpClient){
+  }
+
+  add(category:any){
+    return this.http.post(Endpoint+'/',category,httpOptions)
+  }
+
+
 }
+
+
+
+
+
+
